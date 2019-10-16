1a53ee565b0b7adffff54dae61d76ad805e73d26
Jonathan <jonathan.tecsantos@gmail.com>
2019-10-16 19:20:09 -0300

    commit inicial

---
+++ Exercicio_01_IG/app/src/main/java/com/example/exercicio_01_ig/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.exercicio_01_ig;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
